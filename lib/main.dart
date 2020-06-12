b8681064e6cb6a9fc7bbebff1f574184b02645b4
Jay-Ar Brouillard <jay-ar.brouillard@MacBook-Pro.local>
2020-06-11 20:46:07 -0500

    Added Favorites Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:flutter/semantics.dart';
-
+import './randomWords.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,44 +8,5 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.redAccent),
       home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, item) {
-        if (item.isOdd) return Divider();
-        
-        final index = item ~/ 2;
-
-        if (index >= _randomWordPairs.length) {
-          _randomWordPairs.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_randomWordPairs[index]);
-
-      }
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(pair.asLowerCase,
-      style: TextStyle(fontSize: 18))
-    );
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('WordPair Generator')),
-      body: _buildList());
   }
 }
